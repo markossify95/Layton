2b5f86514c07baacb74f595afe89b4b01be17559
Marko Kostadinović <kostadinovic995@gmail.com>
2018-02-01 02:35:30 +0100

    Parser v1.0 finished

--- parser.py
+++ parser.py
@@ -1,8 +1,29 @@
+import migrator
+import json
+
+short_dict = migrator.generate_prefix_dict()
+print(short_dict)
+
 with open("knjige.txt", "r") as f:
     for line in f:
-        linija = line.split(chr(30))
-        print("LAJNA", linija)
-        for el in linija:
-            print(str(   el[6:]).split(chr(31))   )
+        fields = line.split(chr(30))
+        record = {}
+        for el in fields:
+            tag = el[:3]
+            field_type = set()
+            value = (el[6:]).split(chr(31))
+            polja = migrator.split_field(value)
 
+            for k, v in short_dict.items():
+                for t in v:
+                    if tag in t:
+                        field_type.add(k)
+
+            if field_type is None:
+                continue
+
+            if len(polja) > 0:
+                record[tag] = polja
+
+        print(json.dumps(record, ensure_ascii=False))
         break
