de663743f8be2e1a6cc5a4e1b5381505e2759eb9
Marko Kostadinović <kostadinovic995@gmail.com>
2018-02-13 02:24:31 +0100

    Initial interaction with backend

--- api.py
+++ api.py
@@ -2,14 +2,17 @@ from pprint import pprint
 
 from bson.json_util import (dumps)
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 db = client.library
 books = db.books
 tags = db.tags
+prefixes = db.prefixes
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 
 @app.route('/')
@@ -24,6 +27,7 @@ def get_tags():
 
 
 @app.route('/books', methods=['GET', 'POST'])
+# @cross_origin(supports_credentials=True)
 def filter_books():
     one_book = books.find({"100": {
         "h": "scr",
@@ -32,6 +36,13 @@ def filter_books():
     # one_book = books.find_one({"_id": ObjectId("5a727954d3354a1b3ee82868")})
     pprint(one_book)
     return dumps(one_book, ensure_ascii=False)  # , JSONOptions(json_mode=JSONMode.RELAXED)
+
+
+@app.route('/prefixes', methods=['GET'])
+# @cross_origin(supports_credentials=True)
+def get_prefixes():
+    prefix_list = prefixes.find()
+    return dumps(prefix_list, ensure_ascii=False)  # , JSONOptions(json_mode=JSONMode.RELAXED)
 
 
 @app.route('/<name>')
--- front/js/main.js
+++ front/js/main.js
@@ -32,7 +32,21 @@ var button_id = 2;
 var dropdown_id = 2;
 
 $(document).ready(function () {
+    var prefixes = [];
+    $.ajax({
+        url: "http://127.0.0.1:8080/prefixes",
+        dataType: 'json',
+        type: 'get',
+        crossDomain: true,
+        contentType: 'application/json; charset=utf-8',
+        success: function (data) {
+            prefixes = data;
+        }
+    });
+
+
     $("button[name='adder']").click(function () {
+        console.log(prefixes['0']);
         var tempNode = document.querySelector("div[data-type='template']").cloneNode(true); //true for deep clone
         // console.log(tempNode.find('.logical'));
         $(tempNode).find('.logical').attr('id', 'l' + button_id++);
@@ -41,8 +55,28 @@ $(document).ready(function () {
         $(tempNode).find('.criteria').attr('id', 'dd' + dropdown_id++);
         $(tempNode).find('.criteria').text('Choose criteria');
         $(tempNode).find('.criteria').val('Choose criteria');
+        $.each(prefixes['0'], function (k, v) {
+            var dropdown = $(tempNode).find('.dropdown-menu');
+            $(dropdown).append("<a class='dropdown-item' href='#'>" + v + "</a>");
+        });
         tempNode.style.display = "block";
         document.querySelector("div[data-type='template']").parentNode.appendChild(tempNode);
         $("html, body").animate({scrollTop: $(document).height()}, 500);
     });
+
+    //ajax function OVA TRAZI IZMENU I PRAVILNO PAKOVANJE U JSON
+    $("button[name='searcher']").click(function () {
+        var query = "";
+        var all_divs = document.getElementsByClassName("query-container");
+        // console.log(all_divs);
+        for (i = 1; i < all_divs.length; i++) {
+            var d = $(all_divs[i]);
+            var btn = $(d.find('.criteria')[0]);
+            var txt = $(d.find('.query-text')[0]);
+            var logic = $(d.find('.logical')[0]);
+            query += " " + btn.val() + " " + txt.val() + " " + logic.val();
+        }
+        console.log(query);
+    });
 });
+
